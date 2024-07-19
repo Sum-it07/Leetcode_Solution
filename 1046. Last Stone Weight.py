b8ab989395a803a6fd646dc31689532d44e4a124
Sumit Shrestha <156155151+Sum-it07@users.noreply.github.com>
2024-07-19 09:04:58 +0530

    Create 1046. Last Stone Weight.py

---
+++ 1046. Last Stone Weight.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        while len(stones) > 1:
+            stones.sort()
+            heaviest = stones.pop()
+            second_heaviest = stones.pop()
+            if heaviest != second_heaviest:
+                stones.append(heaviest - second_heaviest)
+        return stones[0] if stones else 0

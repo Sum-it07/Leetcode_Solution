06d5e2183afa9050deee5b27e0dc6219cd77fc66
Sumit Shrestha <156155151+Sum-it07@users.noreply.github.com>
2024-07-24 22:59:47 +0530

    Update 1.TWO SUM.c

--- 1.TWO SUM.c
+++ 1.TWO SUM.c
@@ -1,7 +1,4 @@
 1.TWO SUM
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     for (int i = 0; i < numsSize; i++) {
         for (int j = i + 1; j < numsSize; j++) {

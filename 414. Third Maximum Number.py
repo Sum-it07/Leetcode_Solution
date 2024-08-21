a1275b8c70683e2ef197d5cec839feeb16059445
Sumit Shrestha <156155151+Sum-it07@users.noreply.github.com>
2024-08-22 00:38:43 +0530

    Create 414. Third Maximum Number.py

---
+++ 414. Third Maximum Number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        a=sorted(list(set(nums)))
+        if len(a)<3:
+            return a[-1]
+        else:
+            return a[-3]
